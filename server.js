68a8a949a41778655c71747a530de25bcd6d3fba
Veit Hiller <veithiller@me.com>
2026-02-22 14:56:05 +0100

    server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const session = require('express-session');
 const pgSession = require('connect-pg-simple')(session);
 const bcrypt = require('bcryptjs');
-const { pool } = require('./db');
+const pool = require('./db');
 
 require('dotenv').config();
 
